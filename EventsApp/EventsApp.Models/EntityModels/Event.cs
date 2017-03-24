a7870491963942523bf2504bbf75ede9a3e82df5
Галя Георгиева <alice.gospodinova@gmail.com>
2017-03-24 10:59:52 +0200

    Initial setup for Events application - ASP.NET MVC project

---
+++ EventsApp/EventsApp.Models/EntityModels/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventsApp.Models.EntityModels
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string  Title { get; set; }
+
+        public int UserID { get; set; }
+
+        [ForeignKey("UserID")]
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
